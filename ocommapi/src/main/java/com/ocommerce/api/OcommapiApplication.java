e30bd7d2e4ad723436cf7e19fbc26e2a17cc514a
handsonprogrammer <handsonprogrammer@gmail.com>
2025-01-15 11:17:47 -0500

    base project

---
+++ ocommapi/src/main/java/com/ocommerce/api/OcommapiApplication.java
@@ -0,0 +1,13 @@
+package com.ocommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcommapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcommapiApplication.class, args);
+	}
+
+}
